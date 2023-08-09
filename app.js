d63f231ec80bb2c5100e362a9482bca7f09d703a
Hailey Reich <hailey@reich-family.com>
2023-08-08 23:40:36 -0500

    configure lastfm get request

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 import bodyParser from "body-parser";
 import express from "express";
 import https from "https";
+import dotenv from "dotenv";
+
 
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
+dotenv.config();
+const lastfmKey = process.env.LASTFM_API_KEY;
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -16,4 +22,29 @@ app.listen(3000, function() {
 
 app.get("/", function(req, res){
     res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/", function(req,res){
+    console.log("Post request received");
+    const query = req.body.fmUsername;
+    const lastfmURL = "https://ws.audioscrobbler.com/2.0/?method=user.getTopTracks&user="+query+"&api_key="+lastfmKey+"&format=json&limit=3";
+    https.get(lastfmURL, "JSON", function(response){
+        console.log(response.statusCode);
+        var data;
+        response.on("data", function(chunk){
+            if (!data) {
+                data = chunk;
+            }
+            else {
+                data += chunk;
+            }
+        });
+        response.on("end", function() {
+            const lastfmData = JSON.parse(data);
+            console.log(lastfmData);
+            const track1 = lastfmData.toptracks.track[0].name;
+            res.write("<p> The number one track is "+track1+".</p>");
+            res.send(); 
+        });
+    })
 })
